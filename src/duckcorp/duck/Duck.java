26716c47ca06f109d4b105232c0b1270af8a5480
Theor31 <theo.raban@gmail.com>
2026-04-01 09:33:49 +0200

    feat: equals(Object) et hashCode() dans Duck

--- src/duckcorp/duck/Duck.java
+++ src/duckcorp/duck/Duck.java
@@ -9,7 +9,7 @@ package duckcorp.duck;
  *   - Implémentez les méthodes abstraites dans les sous-classes
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public abstract class Duck {
+public abstract class Duck implements Qualifiable {
 
     private static int counter = 0;
 
@@ -38,18 +38,29 @@ public abstract class Duck {
     // --- TODO : equals et hashCode ---
 
     /**
-     * Deux canards sont égaux si et seulement si ils ont le même identifiant.
-     * TODO : implémentez equals() en vous basant uniquement sur le champ id.
+     * Vérifie l'égalité entre deux canards basée uniquement sur leur identifiant.
+     * Deux canards sont égaux si ils ont le même identifiant unique
+     *
+     * @param o l'objet à comparer avec ce canard
+     * @return true si l'objet est un Duck avec le même identifiant, false sinon
      */
     @Override
     public boolean equals(Object o) {
-        throw new UnsupportedOperationException("TODO : Duck.equals()");
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Duck duck = (Duck) o;
+        return id.equals(duck.id);
     }
 
-    /** TODO : implémentez hashCode() de façon cohérente avec equals(). */
+    /**
+     * Retourne le code de hachage basé uniquement sur l'identifiant du canard.
+     * deux canards égaux auront toujours le même code de hachage.
+     *
+     * @return le code de hachage basé sur l'identifiant unique du canard
+     */
     @Override
     public int hashCode() {
-        throw new UnsupportedOperationException("TODO : Duck.hashCode()");
+        return id.hashCode();
     }
 
     // --- toString fourni ---
